6878cbffb0c433bced25c0ca0298143d34281abd
Stephen Yates <SYatesSoftware@gmail.com>
2020-08-28 16:14:03 +0100

    updated logging for visit api

--- DFC.Api.Visits.UnitTests/ServiceTests/Neo4JServiceTests.cs
+++ DFC.Api.Visits.UnitTests/ServiceTests/Neo4JServiceTests.cs
@@ -4,6 +4,7 @@ using DFC.ServiceTaxonomy.Neo4j.Commands;
 using DFC.ServiceTaxonomy.Neo4j.Commands.Interfaces;
 using DFC.ServiceTaxonomy.Neo4j.Services;
 using FakeItEasy;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using System;
 using System.Threading.Tasks;
@@ -16,13 +17,19 @@ namespace DFC.Api.Visits.UnitTests.ServiceTests
         private readonly IServiceProvider provider;
         private readonly IGraphDatabase graphDatabase;
         private readonly IOptions<VisitSettings> settings;
+        private readonly ILoggerFactory logFactory;
+        private readonly FakeLogger log;
 
         public Neo4JServiceTests()
         {
             provider = A.Fake<IServiceProvider>();
             graphDatabase = A.Fake<IGraphDatabase>();
+            logFactory = A.Fake<ILoggerFactory>();
+            log = A.Fake<FakeLogger>();
+
             A.CallTo(() => graphDatabase.Run(A<ICommand[]>.Ignored)).Returns(Task.CompletedTask);
             A.CallTo(() => provider.GetService(typeof(ICustomCommand))).Returns(new CustomCommand());
+            A.CallTo(() => logFactory.CreateLogger(A<string>.Ignored)).Returns(log);
             settings = Options.Create(new VisitSettings
             {
                 RetentionInDays = 90,
@@ -42,23 +49,49 @@ namespace DFC.Api.Visits.UnitTests.ServiceTests
                     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
             };
 
-            var service = new Neo4JService(graphDatabase, provider, settings);
+            var service = new Neo4JService(graphDatabase, provider, settings, logFactory);
             await service.InsertNewRequest(model).ConfigureAwait(false);
             A.CallTo(() => graphDatabase.Run(A<ICommand[]>.Ignored)).MustHaveHappened();
+            A.CallTo(() => log.Log(LogLevel.Information, A<Exception>.Ignored, A<string>.Ignored))
+                .MustHaveHappened(2, Times.Exactly);
         }
 
         [Fact]
         public async Task WhenCreateVisitRequestNullThrowException()
         {
-            var service = new Neo4JService(graphDatabase, provider, settings);
+            var service = new Neo4JService(graphDatabase, provider, settings, logFactory);
 
-            await Assert.ThrowsAsync<ArgumentNullException>(async () => await service.InsertNewRequest(null).ConfigureAwait(false)).ConfigureAwait(false);
+            await Assert
+                .ThrowsAsync<ArgumentNullException>(async () =>
+                    await service.InsertNewRequest(null).ConfigureAwait(false)).ConfigureAwait(false);
         }
 
         [Fact]
         public async Task WhenSettingsNullThrowException()
         {
-            Assert.Throws<ArgumentNullException>(() => new Neo4JService(graphDatabase, provider, null));
+            Assert.Throws<ArgumentNullException>(() => new Neo4JService(graphDatabase, provider, null, logFactory));
+        }
+
+        [Fact]
+        public async Task WhenInsertNewRequestFailsThenLogError()
+        {
+            var model = new CreateVisitRequest
+            {
+                SessionId = Guid.NewGuid(),
+                RequestPath = "/",
+                Referer = "/",
+                VisitTime = DateTime.UtcNow,
+                UserAgent =
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+            };
+
+            A.CallTo(() => graphDatabase.Run(A<ICommand[]>.Ignored)).Throws(new Exception());
+
+            var service = new Neo4JService(graphDatabase, provider, settings, logFactory);
+            await service.InsertNewRequest(model).ConfigureAwait(false);
+            A.CallTo(() => graphDatabase.Run(A<ICommand[]>.Ignored)).MustHaveHappened();
+            A.CallTo(() => log.Log(LogLevel.Error, A<Exception>.Ignored, A<string>.Ignored))
+                .MustHaveHappened(1, Times.Exactly);
         }
     }
 }
